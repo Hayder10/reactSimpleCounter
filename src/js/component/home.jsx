af9ab002f519bbb94b3653503e1c2da63a6307fd
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-07-07 21:41:20 +0000

    static html code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid bg-dark">
+			<div className="clockBox">
+				<div className="digitBox" style={{width: "150px"}}><FontAwesomeIcon icon={faClock}/></div>
+				<div className="digitBox">9</div>
+				<div className="digitBox">9</div>
+				<div className="digitBox">9</div>
+				<div className="digitBox">9</div>
+				<div className="digitBox">9</div>
+				<div className="digitBox">9</div>
+			</div>
 		</div>
 	);
 };
